93c4a364a86d85e381e5d0343be9b30983f7af66
CorruptedJam <144101585+CorruptedJam@users.noreply.github.com>
2023-09-13 23:11:03 +1000

    Scripts

---
+++ Assets/_scripts/boss npc/Don_Calamari_ATK.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+//Lexan (Xan) Hewett (ㆆ_ㆆ)
+public class Don_Calamari_ATK : MonoBehaviour
+{
+ // we need to discuss this  way more honestly    
+}
