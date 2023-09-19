84340de8bafde9b993308f61493277b26e8c4330
CorruptedJam <lexanhewett@gmail.com>
2023-09-19 20:12:52 +1000

    UM idk what is what i think i deleted monkeys

---
+++ Assets/_scripts/tank lizard/LIZARDmovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+// XAN (ಥ_ಥ)
+public class LIZARDmovement : MonoBehaviour
+{
+    public float m_CloseDistance = 20f;
+    public Transform m_Turret;
+    private GameObject m_Player;
+    private NavMeshAgent m_NavAgent;
+    private Rigidbody m_Rigidbody;
+    private bool m_Follow;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+        if (m_Follow == false)
+            return;
+
+        float distance = (m_Player.transform.position - transform.position).magnitude;
+        if (distance > m_CloseDistance)
+        {
+            m_NavAgent.SetDestination(m_Player.transform.position);
+            m_NavAgent.isStopped = false;
+        }
+        else
+        {
+            m_NavAgent.isStopped = true;
+        }
+        if (m_Turret != null)
+        {
+            m_Turret.LookAt(m_Player.transform);
+        }
+    }
+    private void Awake()
+    {
+        m_Player = GameObject.FindGameObjectWithTag("Player");
+        m_NavAgent = GetComponent<NavMeshAgent>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_Follow = false;
+    }
+    private void OnEnable()
+    {
+        m_Rigidbody.isKinematic = false;
+    }
+    private void OnDisable()
+    {
+        m_Rigidbody.isKinematic = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_Follow = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            m_Follow = false;
+        }
+    }
+}
